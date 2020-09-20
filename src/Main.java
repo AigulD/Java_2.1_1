884ed997f3b066e8aecdffa0c47c324f173b5360
AigulD <70228729+AigulD@users.noreply.github.com>
2020-09-20 20:36:53 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        long ticket_price = 7430;
-        int one_mile_cost = 20;
-        long total_miles_accrued = ticket_price / one_mile_cost;
-        System.out.println(total_miles_accrued);
+        long ticketPrice = 7430;
+        int oneMileCost = 20;
+        long totalMilesAccrued = ticketPrice / oneMileCost;
+        System.out.println(totalMilesAccrued);
     }
 }
